df62d55bd6d0617b772525b8e602cbcdfc8f0ee5
Erijon Zhuta <erijonzhutaa@gmail.com>
2022-12-09 16:03:49 +0100

    refactory ShopsCard

--- components/ShopsCard.js
+++ components/ShopsCard.js
@@ -5,15 +5,11 @@ function ShopsCard({ image, text, width, height, onClickHandler }) {
   //   console.log("adsf");
   // };
   return (
-    <div className="flex flex-col items-center bg-white border-solid space-y-4 pt-12 pb-1 rounded-3xl shadow-md">
-      <Image
-        onClick={onClickHandler}
-        src={image}
-        alt="text"
-        width={width}
-        height={height}
-        style={{ cursor: "pointer" }}
-      />
+    <div
+      className="flex flex-col items-center bg-white border-solid space-y-4 pt-12 pb-1 rounded-3xl shadow-md cursor-pointer hover:drop-shadow-xl"
+      onClick={onClickHandler}
+    >
+      <Image src={image} alt="text" width={width} height={height} />
       <p className="text-center text-2xl py-8">{text}</p>
     </div>
   );
